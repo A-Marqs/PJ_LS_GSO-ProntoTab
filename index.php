c7cddad8e57c2276604e5e4ad253f5b151a7e10b
Aldrin Marques <aldrin.marques@hotmail.com>
2020-10-25 18:24:58 -0300

    .

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login ProntoTab</title>
 
-    <link href="styles/main.css" rel="stylesheet"> <!--Estilo CSS-->
+    <link href='../styles/login_page.css' rel="stylesheets">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet"> <!-- Fonte Principal -->
 
 </head>
