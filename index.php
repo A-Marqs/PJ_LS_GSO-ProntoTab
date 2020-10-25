46e6618e6949cdcbd99ed7df8adb5f34ddaa014a
Aldrin Marques <aldrin.marques@hotmail.com>
2020-10-25 18:18:39 -0300

    .

--- index.php
+++ index.php
@@ -5,37 +5,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login ProntoTab</title>
 
-    <link href="./styles/main.css" rel="stylesheet"> <!--Estilo CSS-->
+    <link href="styles/main.css" rel="stylesheet"> <!--Estilo CSS-->
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet"> <!-- Fonte Principal -->
-    <link href="/scripts/login_action.js">
-
-    <script>
-        function teste() { 
-            console.log("teste");
-        }
-    </script>
 
 </head>
 <body>
-    <h1>ProntoTab</h1>
+    <div class="container">
+        <div id="logo_box"> 
+            <h1>ProntoTab</h1>
+        </div>
 
-    <div id="login_form">
-        <form id="form" method="post" action="./php/authentication.php">
-            <!--Usuário-->
-            <div>
-                <label for="user_name"> Usuário: </label>
-                <input type="text" id="user_name" name="user_name"/>
-            </div>
+        <!--Formulário de Login-->
+        <div id="login_form">
+            <form id="userdata" method="post" action="./php/authentication.php">
+                <!--Usuário-->
+                <div>
+                    <label for="user_name">Usuário:</label>
+                    <input type="text" id="user_name" name="user_name"/>
+                </div>
 
-            <!--Senha-->
-            <div>
-                <label for="password"> Senha: </label>
-                <input type="text" id="password" name="password"/>
-            </div>
+                <!--Senha-->
+                <div>
+                    <label for="password">Senha:</label>
+                    <input type="text" id="password" name="password"/>
+                </div>
 
-            <!--Autenticação de Login-->
-            <button type="submit"> Fazer Login </button>    
-        </form>
+                <!--Autenticação de Login-->
+                <button type="submit"> Fazer Login </button>    
+            </form>
+        </div>
     </div>
 
 </body>
