8165be217ea04dbc443db50b50629f32f793e874
Aldrin Marques <aldrin.marques@hotmail.com>
2020-10-25 19:15:51 -0300

    Finalização do CSS
    foi terminado o CSS da pagina de login.

--- index.php
+++ index.php
@@ -5,28 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login ProntoTab</title>
 
-    <link href='../styles/login_page.css' rel="stylesheets">
+    <link href='styles/login_page.css' rel="stylesheet"> <!-- Estilo CSS-->
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet"> <!-- Fonte Principal -->
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet"> <!-- Fonte Secundária -->
 
 </head>
 <body>
     <div class="container">
-        <div id="logo_box"> 
+        <div class="logo_box"> 
             <h1>ProntoTab</h1>
         </div>
 
         <!--Formulário de Login-->
-        <div id="login_form">
+        <div class="login_form">
             <form id="userdata" method="post" action="./php/authentication.php">
                 <!--Usuário-->
-                <div>
-                    <label for="user_name">Usuário:</label>
+                <div class="user">
+                    <label for="user_name">usuário:</label>
                     <input type="text" id="user_name" name="user_name"/>
                 </div>
 
                 <!--Senha-->
-                <div>
-                    <label for="password">Senha:</label>
+                <div class="pass">
+                    <label for="password">senha:</label>
                     <input type="text" id="password" name="password"/>
                 </div>
 
