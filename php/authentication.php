ca43e8f16e06a299d28005e3ffe619f619404b28
Victor Uziel <uzielteixeiraalbuquerque@gmail.com>
2020-10-28 21:41:38 -0300

    UPDATE

--- php/authentication.php
+++ php/authentication.php
@@ -31,21 +31,21 @@
     //$testeUser = 'testes'; // Usuário de testes
     //$testePass = '12345';  // Senha do usuário de testes 
 
-    if ($user == $testeUser && $pass == $testePass)
-    {
-    	session_start();
-    	$_SESSION['main'] = true;
-        echo "
-        <script language='javascript' type='text/javascript'>
-        window.alert('Logado como: usuário de testes!'); 
-        window.location.href='../prontotab.php'; 
-        </script>";
-    }
-    else
-    {
-        echo "<script language='javascript' type='text/javascript'>
-        window.alert('Usuário e/ou senha incorretos'); 
-        window.location.href='../index.php'; 
-        </script>";
-    }
+    //if ($user == $testeUser && $pass == $testePass)
+    //{
+    //	session_start();
+    //	$_SESSION['main'] = true;
+    //    echo "
+      //  <script language='javascript' type='text/javascript'>
+        //window.alert('Logado como: usuário de testes!'); 
+        //window.location.href='../prontotab.php'; 
+        //</script>";
+    //}
+    //else
+    //{
+    //    echo "<script language='javascript' type='text/javascript'>
+    //    window.alert('Usuário e/ou senha incorretos'); 
+    //    window.location.href='../index.php'; 
+    //    </script>";
+    //}
 ?>
