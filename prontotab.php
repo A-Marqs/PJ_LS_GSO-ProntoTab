7959a44522b0c6c10570545fa4d482d3b93afda4
Victor Uziel <uzielteixeiraalbuquerque@gmail.com>
2020-10-29 22:35:27 -0300

    .

--- prontotab.php
+++ prontotab.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login ProntoTab</title>
 
-    <link href="./styles/main.css" rel="stylesheet"> <!--Estilo CSS-->
+    <link href="/styles/login_page.css" rel="stylesheet"> <!--Estilo CSS-->
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet"> <!-- Fonte Principal -->
 </head>
 <body>
     <h1>ProntoTab</h1>
 
-    <p>pagina principal</p>
+    <p>Bem vindo ao Sistema!</p>
 </body>
 </html>
